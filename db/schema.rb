445b2426823e20b67ea9147ae48cd72cb226430a
Greg <gkenenitz@jibe.com>
2015-07-23 16:23:29 -0400

    Adds author column to entries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723150920) do
+ActiveRecord::Schema.define(version: 20150723201509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150723150920) do
     t.string "tags"
     t.string "md5"
     t.string "source"
+    t.string "author"
   end
 
 end
