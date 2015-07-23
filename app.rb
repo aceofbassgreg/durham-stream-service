dcb890acbdc1ca2af13f7cf6df33610f9c780c6b
Greg <gkenenitz@jibe.com>
2015-07-23 14:55:06 -0400

    validates presence of entry attributes, adds /submit endpoint to service

--- app.rb
+++ app.rb
@@ -7,9 +7,11 @@ require './app/models/entry'
 
 Bundler.require
 
+class InvalidEntry < StandardError; end
+
 module RTPEventService
   class App < Sinatra::Application
-    
+
     register Sinatra::ActiveRecordExtension
     # configure do
     #   disable :method_override
@@ -23,6 +25,16 @@ module RTPEventService
     # end
 
     # use Rack::Deflater
+
+
+    # @entry.save || raise InvalidEntry
+    post '/submit' do
+      @entry = Entry.new(params[:entry])
+      @entry.save || raise InvalidEntry
+    rescue InvalidEntry
+      puts "Entry was not formatted properly!"
+      status 422
+    end
 
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,6 @@
 class Entry < ActiveRecord::Base
 
+  validates_presence_of :text_to_display, :text_created_at,
+                    :tags, :md5, :source
+
 end
